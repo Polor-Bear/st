d48cfbeeed019b70d9a19bc949dcf4aa7ca85632
Polar Bear <1097041650@qq.com>
2022-04-10 10:15:55 +0800

    change font

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Source Code Pro:pixelsize=20:antialias=true:autohint=true";
+static char *font = "cascadia code:pixelsize=18:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
